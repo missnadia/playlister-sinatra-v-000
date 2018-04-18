7aa3c55db45830eb9ae50159a6e7fd85a885c126
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-18 20:06:14 +0000

    automatically backed up by learn

--- app/models/song_genre.rb
+++ app/models/song_genre.rb
@@ -2,4 +2,12 @@ class SongGenre < ActiveRecord::Base
 
   belongs_to :song
   belongs_to :genre
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.find{ |artist| artist.slug == slug}
+  end
 end
