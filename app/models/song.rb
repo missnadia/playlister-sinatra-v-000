5309d1e07e9d0f7388febbc6763a4005f1926432
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-18 20:41:02 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,17 @@
-class Song::Slug < ActiveRecord::Base
+class Song < ActiveRecord::Base
 
-  extend Slug
-  include Slug
+  extend Slug::ClassMethods
+  include Slug::InstanceMethods
 
   has_many :song_genres
   belongs_to :artist
   has_many :genres, through: :song_genres
+
+  def self.find_by_slug(slug)
+    self.all.find{ |c| c.slug == slug}
+  end
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
 end
