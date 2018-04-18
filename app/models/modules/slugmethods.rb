4a90725eafb7ee35ebe512c34b141ca18a208765
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-18 20:44:07 +0000

    automatically backed up by learn

--- app/models/modules/slugmethods.rb
+++ app/models/modules/slugmethods.rb
@@ -3,11 +3,8 @@ module SlugMethods
     def self.find_by_slug(slug)
       self.all.find{ |c| c.slug == slug}
     end
-  end
 
-  module InstanceMethods
     def slug
       name.downcase.gsub(" ","-")
     end
-  end
 end
