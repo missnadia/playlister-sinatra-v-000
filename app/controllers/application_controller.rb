c5020575996c9782efeac1e9d872635e3e75d248
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-18 19:22:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,30 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/songs' do
+    
+  end
+
+  post '/songs' do
+
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  '/genres'
+
+  '/artists'
+
+  patch '/songs/:slug' do
+    
+  end
+
+  '/artists/:slug'
+
+  '/genres/:slug'
+
+  '/songs/new'
+
+  '/songs/:slug/edit'
 end
