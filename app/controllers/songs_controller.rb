899293d77a0af572cd29a37bb1deb0ff01abf7f0
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-18 20:24:30 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,26 @@
 class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :"/songs/index"
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    if !params["song"]["name"].empty?
+      @song.name << Song.create(name: params["song"]["name"])
+    end
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect :"/songs/#{@song.slug}"
+  end
+
+  patch '/songs/:slug' do
+
+  end
+
+  '/songs/new'
+
+  '/songs/:slug/edit'
 end
