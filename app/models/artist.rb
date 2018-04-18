a468a02dd05f19ca6596b080327372df6eef30c1
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-18 20:42:18 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ActiveRecord::Base
 
-  extend Slug
-  include Slug
+  extend Slug::ClassMethods
+  include Slug::InstanceMethods
 
   has_many :songs
   has_many :genres, through: :songs
